00e43595038be7a5a3df3b5662e56099b686aa21
Gene Liverman <gliverma@westga.edu>
2014-04-23 12:33:55 -0400

    Added Apache http and https vhosts to uwg-dw01

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,26 @@
 node 'uwg-dw01' {
   include degreeworks
+
+  class {'apache':
+    default_vhost     => false,
+  }
+
+  ::apache::vhost { 'default-vhost':
+    #ip              => '10.10.5.44'
+    port            => 80,
+    priority        => '00',
+    docroot         => '/var/www/degreeworks',
+    options         => [ '-Indexes', 'FollowSymLinks' ],
+  }
+
+  ::apache::vhost { 'default-ssl-vhost':
+    #ip              => '10.10.5.44'
+    port            => 443,
+    priority        => '00',
+    docroot         => '/var/www/degreeworks',
+    options         => [ '-Indexes', 'FollowSymLinks' ],
+    ssl             => true,
+    ssl_protocol    => '-ALL +SSLv3 +TLSv1',
+    ssl_cipher      => 'HIGH:MEDIUM:RC4+RSA:!LOW:!SSLv2:!TLSv1!aNULL:!MD5:!EXPORT',
+  }
 }
