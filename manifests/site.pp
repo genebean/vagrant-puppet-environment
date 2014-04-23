e8d8dd27895fa8b708e4d41b0b7e60b17a3381b3
Gene Liverman <gliverma@westga.edu>
2014-04-23 15:33:33 -0400

    Added settings per the Degree Works install guide. Moved common lines to variables. Fixed => alignment on one line.

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,15 +2,23 @@ node 'uwg-dw01' {
   include degreeworks
 
   class {'apache':
-    default_vhost     => false,
+    default_vhost   => false,
   }
+
+  $httpoptions = [ 'Indexes', 'FollowSymLinks', 'ExecCGI' ]
+  $dwhandlers  = [{ handler => 'cgi-script', extensions => ['.cgi']}]
 
   ::apache::vhost { 'default-vhost':
     #ip              => '10.10.5.44'
     port            => 80,
     priority        => '00',
     docroot         => '/var/www/degreeworks',
-    options         => [ '-Indexes', 'FollowSymLinks' ],
+    options         => $httpoptions,
+    addhandlers     => $dwhandlers,
+    allow_override  => ['All'],
+    order           => 'Allow,Deny',
+    allow           => 'from all',
+    directoryindex  => 'default.html',
   }
 
   ::apache::vhost { 'default-ssl-vhost':
@@ -18,7 +26,13 @@ node 'uwg-dw01' {
     port            => 443,
     priority        => '00',
     docroot         => '/var/www/degreeworks',
-    options         => [ '-Indexes', 'FollowSymLinks' ],
+    options         => $httpoptions,
+    addhandlers     => $dwhandlers,
+    allow_override  => ['All'],
+    order           => 'Allow,Deny',
+    allow           => 'from all',
+    directoryindex  => 'default.html',
+
     ssl             => true,
     ssl_protocol    => '-ALL +SSLv3 +TLSv1',
     ssl_cipher      => 'HIGH:MEDIUM:RC4+RSA:!LOW:!SSLv2:!TLSv1!aNULL:!MD5:!EXPORT',
