d656b02bdb60dd320b1262a04926d39fee33e3d7
Gene Liverman <gliverma@westga.edu>
2014-05-02 13:54:28 -0400

    Updated SSL config for DegreeWorks. Added several nodes for servers that the web team has.

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,7 +45,96 @@ node 'uwg-dw01' {
     ],
 
     ssl             => true,
-    ssl_protocol    => '-ALL +SSLv3 +TLSv1',
-    ssl_cipher      => 'HIGH:MEDIUM:RC4+RSA:!LOW:!SSLv2:!TLSv1!aNULL:!MD5:!EXPORT',
+    ssl_protocol    => 'All -SSLv2',
+    ssl_cipher      => 'ALL:!ADH:!EXPORT:!SSLv2:RC4+RSA:+HIGH:+MEDIUM:+LOW',
+  }
+}
+
+node 'dev01.www.westga.edu' {
+  ::network::if::static { 'eth1':
+    ensure          => 'up',
+    macaddress      => '00:50:56:8E:12:64',
+    ipaddress       => '10.10.5.47',
+    netmask         => '255.255.255.0',
+  }
+}
+
+node 'prd01.www.westga.edu' {
+  ::network::if::static { 'eth1':
+    ensure          => 'up',
+    macaddress      => '00:50:56:8E:2A:63',
+    ipaddress       => '10.10.5.48',
+    netmask         => '255.255.255.0',
+  }
+}
+
+node 'dev01.staff.westga.edu' {
+  ::network::if::static { 'eth1':
+    ensure          => 'up',
+    macaddress      => '00:50:56:8e:59:f9',
+    ipaddress       => '10.10.5.49',
+    netmask         => '255.255.255.0',
+  }
+}
+
+node 'dev01.webapps.westga.edu' {
+  ::network::if::static { 'eth1':
+    ensure          => 'up',
+    macaddress      => '00:50:56:8e:38:1c',
+    ipaddress       => '10.10.5.50',
+    netmask         => '255.255.255.0',
+  }
+}
+
+node 'dev01.cms.westga.edu' {
+  ::network::if::static { 'eth1':
+    ensure          => 'up',
+    macaddress      => '00:50:56:8e:02:16',
+    ipaddress       => '10.10.5.51',
+    netmask         => '255.255.255.0',
+  }
+
+  class {'apache':
+    default_vhost   => false,
+  }
+
+  $httpoptions = [ '-Indexes', 'FollowSymLinks' ]
+
+  ::apache::vhost { 'default-vhost':
+    ip              => '10.10.5.51',
+    port            => 80,
+    priority        => '00',
+    docroot         => '/var/www/html',
+    options         => $httpoptions,
+    directories     => [
+      { path           => '/var/www/html',
+        directoryindex => 'index.html index.htm index.php',
+
+        allow_override => ['All'],
+        order          => 'Allow,Deny',
+        allow          => 'from all',
+      },
+    ],
+  }
+
+  ::apache::vhost { 'default-ssl-vhost':
+    ip              => '10.10.5.51',
+    port            => 443,
+    priority        => '00',
+    docroot         => '/var/www/html',
+    options         => $httpoptions,
+    directories     => [
+      { path           => '/var/www/html',
+        directoryindex => 'index.html index.htm index.php',
+
+        allow_override => ['All'],
+        order          => 'Allow,Deny',
+        allow          => 'from all',
+      },
+    ],
+
+    ssl             => true,
+    ssl_protocol    => 'All -SSLv2',
+    ssl_cipher      => 'ALL:!ADH:!EXPORT:!SSLv2:RC4+RSA:+HIGH:+MEDIUM:+LOW',
   }
 }
