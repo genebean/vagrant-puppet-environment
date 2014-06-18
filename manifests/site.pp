3a79666d13a3c3a090150c912d61204f7683a273
Gene Liverman <gliverma@westga.edu>
2014-06-18 15:11:46 -0400

    Added entries for a Wordpress instance

--- manifests/site.pp
+++ manifests/site.pp
@@ -141,12 +141,64 @@ node 'dev01.cms.westga.edu' {
     ssl_cipher      => 'ALL:!ADH:!EXPORT:!SSLv2:RC4+RSA:+HIGH:+MEDIUM:+LOW',
   }
 
+  ::apache::vhost { 'wp-dev.westga.edu-vhost':
+    servername      => 'wp-dev.westga.edu',
+    ip              => '10.10.5.51',
+    port            => 80,
+    priority        => '10',
+    docroot         => '/var/www/wp',
+    docroot_owner   => 'apache',
+    docroot_group   => 'webgroup',
+    options         => $httpoptions,
+    directories     => [
+      { path           => '/var/www/wp',
+        directoryindex => 'index.html index.htm index.php',
+
+        allow_override => ['All'],
+        order          => 'Allow,Deny',
+        allow          => 'from all',
+      },
+    ],
+  }
+
+  ::apache::vhost { 'wp-dev.westga.edu-ssl-vhost':
+    servername      => 'wp-dev.westga.edu',
+    ip              => '10.10.5.51',
+    port            => 443,
+    priority        => '10',
+    docroot         => '/var/www/wp',
+    docroot_owner   => 'apache',
+    docroot_group   => 'webgroup',
+    options         => $httpoptions,
+    directories     => [
+      { path           => '/var/www/wp',
+        directoryindex => 'index.html index.htm index.php',
+
+        allow_override => ['All'],
+        order          => 'Allow,Deny',
+        allow          => 'from all',
+      },
+    ],
+
+    ssl             => true,
+    ssl_protocol    => 'All -SSLv2',
+    ssl_cipher      => 'ALL:!ADH:!EXPORT:!SSLv2:RC4+RSA:+HIGH:+MEDIUM:+LOW',
+  }
+
+  file { '/var/www/wp':
+    ensure  => 'directory',
+    owner   => 'apache',
+    group   => 'webgroup',
+    mode    => '2775',
+  }
+
+
   include apache::mod::php
   class {'apache::mod::info':
     #restrict_access => false,
     allow_from      => [
-      '160.10.36', 
-      '160.10.38', 
+      '160.10.36',
+      '160.10.38',
       '127.0.0.1',
     ],
   }
